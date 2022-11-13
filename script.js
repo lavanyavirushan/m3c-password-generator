6bc1ea15d86c8ff00b95b95ce5a6fc2c4f40bbff
Lavanya Virushan <lavanya.virushan@gmail.com>
2022-11-13 12:26:38 -0500

    corrected html and added promts to JavaScript

--- script.js
+++ script.js
@@ -1,73 +1,55 @@
 // Assignment code here
-const passwordEl = document.getElementById('password'); 
-const lengthEl = document.getElementById('length'); 
-const lowercaseEl = document.getElementById('lowercase'); 
-const uppercaseEl = document.getElementById('uppercase'); 
-const numbersEl = document.getElementById('numbers'); 
-const specialEl = document.getElementById('special'); 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
+const passwordText = document.querySelector("#password");
 
 
-const uppercaseCharCodes = arrayLowToHigh (65, 90);
-const lowercaseCharCodes = arrayLowToHigh (97, 122);
-const numbersCharCodes = arrayLowToHigh (48, 57);
-const specialCharCodes = arrayLowToHigh (33, 47).concat (arrayLowToHigh (58, 64)).concat (arrayLowToHigh (91, 96)).concat (arrayLowToHigh (123, 126));
-
-// Generate event listen
-generateBtn.addEventListener("click", () => {
-  // the + is to parse the length value from string to number
-  const length = +lengthEl.value; 
-  const includeLowercase = lowercaseEl.checked;
-  const includeUppercase = uppercaseEl.checked; 
-  const includeNumbers = numbersEl.checked;  
-  const includeSpecial= specialEl.checked; 
+const lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numberCharacters = "1234567890";
+const specialCharacters = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
 
-  passwordEl.innerText = generatePassword(includeLowercase, includeUppercase, includeNumbers, includeSpecial, length);
-});
-
-// Generate password function
-
-
-function generatePassword (lowercase, uppercase, numbers, special, length) {
-
-  let generatePassword = '';
-
-  const clicked = lowercase + uppercase + numbers + special; 
-
-  // console.log ('clicked:', clicked); 
-
-  const typesArray = [{ lowercase }, { uppercase }, { numbers }, { special }];
-
-  // console.log ('typesArray:', typesArray)
-
-  if (clicked === 0) {
-    return window.alert("Must select atleast one character type!");
-  }
-
-
-
-  console.log ('Button clicked!'); 
-  let charCodes = lowercaseCharCodes 
-  if (uppercase)
-  console.log (lowercaseCharCodes)
-  return "Generated Password";
-}
-
-function arrayLowToHigh (low, high) {
-  const array = []
-  for (let i = low; i <= high; i++) {
-    array.push(i)
-  }
-  return array
-}
+let passwordLength 
+let includeUppercase; 
+let includeLowercase;
+let includeNumbers; 
+let includeSpecial; 
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+    confirmLength();
+    confirmCharacters();
+    
+    var password = generatePassword();
+    
+    // passwordText.value = password;
 }
+
+function confirmCharacters() {
+  includeLowercase = confirm("Would you like to include lowercase letters?");
+  includeUppercase = confirm("Would you like to include uppercase letters?");
+  includeNumbers = confirm("Would you like to include numbers?");
+  includeSpecial = confirm("Would you like to include special characters?");
+  let includeCharacters = includeLowercase + includeUppercase + includeNumbers + includeSpecial; 
+
+  if(includeCharacters<1){
+    alert("At least one character type should be selected!");
+    confirmCharacters();
+  }
+}
+
+function generatePassword() {
+  console.log(passwordLength);
+}
+
+function confirmLength() {
+  passwordLength = prompt("Choose a length of at least 8 characters and no more than 128!");
+
+  if(isNaN(passwordLength) || passwordLength<8 || passwordLength>128){
+    passwordLength = null;
+    confirmLength();
+  } 
+}
+  
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
